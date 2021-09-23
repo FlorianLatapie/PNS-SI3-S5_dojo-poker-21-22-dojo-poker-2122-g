536f6f7001ad5f0087dadce550ec8c11cc8485e6
LoicLeBris <loic.le-bris@etu.univ-cotedazur.fr>
2021-09-23 17:54:19 +0200

    Update ComparateurCarte.java

--- src/com/dojopoker/main/ComparateurCarte.java
+++ src/com/dojopoker/main/ComparateurCarte.java
@@ -4,12 +4,28 @@ public class ComparateurCarte {
 
     public static String compare(Main mainA, Main mainB){
 
-        // carte la plus haute
-        if (mainA.cartes.get(0).valeur > mainB.cartes.get(0).valeur){
-            return "A gagne par carte la plus haute";
+        return compareTotalDesMains(main1, main2);
+    }
+
+    private static String compareTotalDesMains(Main main1, Main main2){
+        int resMain1 = 0, resMain2 = 0;
+
+        for(int i=0; i<main1.getLenghtMain(); i++){
+            if (main1.cartes.get(i).valeur > main2.cartes.get(i).valeur){
+                resMain1++;
+            }
+            else if (main1.cartes.get(i).valeur < main2.cartes.get(i).valeur){
+                resMain2++;
+            }
         }
-        else {
-            return "B gagne par carte la plus haute";
+
+        if(resMain1 == resMain2){
+            return "Egalité !";
+        }
+        else if (resMain1 > resMain2){
+            return "Main 1 gagne !";
+        } else {
+            return "Main 2 gagne !";
         }
     }
 
