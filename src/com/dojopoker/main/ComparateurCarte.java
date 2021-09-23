54c51022eaebdcaac7a74b926fc8bf16f548fb6d
Florian LATAPIE <70631622+FlorianLatapie@users.noreply.github.com>
2021-09-23 18:21:55 +0200

    Merge du commit de loic + légère simplification dans les for

--- src/com/dojopoker/main/ComparateurCarte.java
+++ src/com/dojopoker/main/ComparateurCarte.java
@@ -3,45 +3,13 @@ package com.dojopoker.main;
 public class ComparateurCarte {
 
     public static String compare(Main main1, Main main2){
-
-        return compareTotalDesMains(main1, main2);
-    }
-
-    private static String compareTotalDesMains(Main main1, Main main2){
-        int resMain1 = 0, resMain2 = 0;
-
-        for(int i=0; i<main1.getLenghtMain(); i++){
-            if (main1.cartes.get(i).valeur > main2.cartes.get(i).valeur){
-                resMain1++;
-            }
-            else if (main1.cartes.get(i).valeur < main2.cartes.get(i).valeur){
-                resMain2++;
-            }
-        }
-
-        if(resMain1 == resMain2){
-            return "Egalité !";
-        }
-        else if (resMain1 > resMain2){
-            return "Main 1 gagne !";
-        } else {
-            return "Main 2 gagne !";
-        }
-    }
-
-    public static String compareTotalDesMains(Main main1, Main main2){
         String res;
 
         if((contientPaire(main1) || contientPaire(main2))){
             res=comparePaires(main1,main2);
-        }
-
-        else{
+        } else{
             res=sommeValeurs(main1,main2);
         }
-
-
-
         return res;
     }
 
@@ -52,16 +20,14 @@ public class ComparateurCarte {
         for(int i=0; i<main1.getLenghtMain(); i++){
             if (main1.cartes.get(i).valeur > main2.cartes.get(i).valeur){
                 resMain1++;
-            }
-            else if (main1.cartes.get(i).valeur < main2.cartes.get(i).valeur){
+            } else if (main1.cartes.get(i).valeur < main2.cartes.get(i).valeur){
                 resMain2++;
             }
         }
 
-        if(resMain1 == resMain2){
+        if(resMain1 == resMain2) {
             res = "Egalité !";
-        }
-        else if (resMain1 > resMain2){
+        } else if (resMain1 > resMain2){
             res = "Main 1 gagne !";
         } else {
             res = "Main 2 gagne !";
@@ -71,59 +37,46 @@ public class ComparateurCarte {
     }
 
     public static String comparePaires(Main main1, Main main2){
-        String res;
-
-
         if(contientPaire(main1) && contientPaire(main2)){
             if(valeurPaire(main1)>valeurPaire(main2)){
-                res="Main 1 gagne !";
+                return "Main 1 gagne ! grâce à paire vs paire : "+ valeurPaire(main1);
             }
             else if(valeurPaire(main1)<valeurPaire(main2)){
-                res="Main 2 gagne !";
+                return "Main 2 gagne ! grâce à paire vs paire : "+ valeurPaire(main2);
             }
             else{
-                res="Egalite !";
+                return "Egalite ! de paire";
             }
+        }else if(contientPaire(main1)){
+            return "Main 1 gagne ! grâce à paire : "+ valeurPaire(main1);
+        }else{
+            return "Main 2 gagne ! grâce à paire : "+ valeurPaire(main1);
         }
-
-        else if(contientPaire(main1)){
-            res="Main 1 gagne !";
-        }
-        else{
-            res="Main 2 gagne !";
-        }
-        return res;
     }
 
     public static boolean contientPaire(Main main1){
-        boolean res = false;
-
         for(int i = 0; i<main1.getLenghtMain(); i++){
             for(int j = 0; j< main1.getLenghtMain(); j++){
                 if(i!=j){
                     if(main1.cartes.get(i).valeur==main1.cartes.get(j).valeur) {
-                        res = true;
+                        return true;
                     }
                 }
             }
         }
-
-        return res;
+        return false;
     }
 
     public static int valeurPaire(Main main){
-        int res = 0;
-
         for(int i = 0; i<main.getLenghtMain(); i++){
             for(int j = 0; j< main.getLenghtMain(); j++){
                 if(i!=j){
                     if(main.cartes.get(i).valeur==main.cartes.get(j).valeur) {
-                        res = main.cartes.get(i).valeur;
+                        return main.cartes.get(i).valeur;
                     }
                 }
             }
         }
-
-        return res;
+        return 0;
     }
 }
